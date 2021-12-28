3f643b318c3b40c7f4e2ffb5e507f0f8fad6b0fe
Jan T. Sott <git@idleberg.com>
2021-12-28 01:01:27 +0100

    update tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,25 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
   ],
+  env: {
+      node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:json/recommended-with-comments'
   ],
+  ignorePatterns: [
+    'index.js'
+  ],
+  overrides : [
+    {
+      files: [
+        '*.yaml',
+        '*.yml'
+      ],
+      plugins: ['yaml'],
+      extends: ['plugin:yaml/recommended']
+    }
+]
 };
