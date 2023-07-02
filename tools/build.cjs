0e5c015174553d500e7981c647dfec331c7d4b08
Jan T. Sott <git@idleberg.com>
2023-07-02 14:08:08 +0200

    adjust build config

--- tools/build.cjs
+++ tools/build.cjs
@@ -1,12 +1,14 @@
+const isDevMode = process.env.ESBUILD_WATCH === 'development';
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('esbuild')
   .build({
     bundle: true,
     entryPoints: ['src/index.ts'],
     external: ['vscode'],
-    minify: true,
+    minify: !isDevMode,
     outfile: 'lib/extension.js',
     platform: 'node',
-    sourcemap: true,
-    watch: process.env.NODE_ENV === 'development'
+    sourcemap: !isDevMode,
+    watch: isDevMode
   }).catch(() => process.exit(1))
