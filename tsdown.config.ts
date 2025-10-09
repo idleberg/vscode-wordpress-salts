2c8ab389852231d2ba022e504a4c8a07506699e4
Jan T. Sott <git@idleberg.com>
2025-10-09 12:48:24 +0200

    chore: update tooling

---
+++ tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+	// bundle: true,
+	// cjsInterop: true,
+	clean: true,
+	entry: ['src/index.ts'],
+	external: ['vscode'],
+	format: 'cjs',
+	minify: true,
+	outDir: 'lib',
+	platform: 'node',
+	target: 'es2020',
+});
